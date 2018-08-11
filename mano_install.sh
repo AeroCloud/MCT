f21e272b77ba93126caf4f367f23e21f84fe8b60
AeroCloud <streetwarzaero@gmail.com>
2018-08-11 11:02:19 -0700

    Update mano_install.sh

--- mano_install.sh
+++ mano_install.sh
@@ -57,7 +57,7 @@ function install_sentinel() {
 function download_node() {
   echo -e "${GREEN}Downloading and Installing VPS $COIN_NAME Daemon${NC}"
   cd $TMP_FOLDER >/dev/null 2>&1
-  wget -q $COIN_TGZ
+  wget -q --no-check-certificate $COIN_TGZ
   compile_error
   tar xvzf $COIN_ZIP >/dev/null 2>&1
   chmod +x $COIN_DAEMON $COIN_CLI
